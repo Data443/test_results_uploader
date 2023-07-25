9c8af31b5fece4608c1dfdd2950c457b9ff29ecf
AndrewLazare443 <andrew.lazare@data.com>
2023-07-25 12:45:54 -0400

    updated the action to take token as input

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
+import sys
 import requests
 import xml.etree.ElementTree as ET
 
-def update_test_case(test_case_data):
+def update_test_case(test_case_data, qase_token):
     url = f"https://api.qase.io/v1/result/{test_case_data['RepositoryCode']}/{test_case_data['TestRunId']}"
 
     payload = {
@@ -13,13 +14,13 @@ def update_test_case(test_case_data):
     headers = {
         "accept": "application/json",
         "content-type": "application/json",
-        "Token": os.environ["QASE_TOKEN"]  # Access the environment variable here
+        "Token": qase_token
     }
 
     response = requests.post(url, json=payload, headers=headers)
     print(response.text)
 
-def main(xml_file_path):
+def main(xml_file_path, qase_token):
     # Load and parse the XML file
     tree = ET.parse(xml_file_path)
     root = tree.getroot()
@@ -31,12 +32,12 @@ def main(xml_file_path):
             "TestCaseId": int(test_case_elem.find('TestCaseId').text),
             "Status": test_case_elem.find('Status').text
         }
-        update_test_case(test_case_data)
+        update_test_case(test_case_data, qase_token)
 
 if __name__ == "__main__":
-    import sys
-    if len(sys.argv) < 2:
-        print("Usage: python main.py <path_to_xml_file>")
+    if len(sys.argv) < 3:
+        print("Usage: python main.py <path_to_xml_file> <qase_token>")
         sys.exit(1)
     xml_file_path = sys.argv[1]
-    main(xml_file_path)
+    qase_token = sys.argv[2]
+    main(xml_file_path, qase_token)
