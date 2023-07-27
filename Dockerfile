f2df16385a5609b6d5356a76c66065089e46569f
AndrewLazare443 <andrew.lazare@data.com>
2023-07-27 08:58:15 -0400

    change the way it was called

--- Dockerfile
+++ Dockerfile
@@ -5,12 +5,16 @@ FROM python:3.9-slim
 WORKDIR /app
 
 # Copy the necessary files to the container
-COPY entrypoint.sh /app/
+# COPY entrypoint.sh /app/
 COPY main.py /app/
 COPY requirements.txt /app/
 
 # Install Python dependencies
 RUN pip install --no-cache-dir -r requirements.txt
 
+# Set executable permissions for entrypoint.sh
+# RUN chmod +x /app/entrypoint.sh
+
 # Set the entrypoint for the container
-ENTRYPOINT ["sh", "/app/entrypoint.sh"]
+# ENTRYPOINT ["/app/entrypoint.sh"]
+ENTRYPOINT ["python", "/app/main.py"] 
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import requests
 import xml.etree.ElementTree as ET
 
@@ -20,7 +19,7 @@ def update_test_case(test_case_data, qase_token):
     response = requests.post(url, json=payload, headers=headers)
     print(response.text)
 
-def main(xml_file_path, qase_token):
+def main(xml_file_path, qase_token):  # Add xml_file_path as an argument
     # Load and parse the XML file
     tree = ET.parse(xml_file_path)
     root = tree.getroot()
@@ -40,4 +39,51 @@ if __name__ == "__main__":
         sys.exit(1)
     xml_file_path = sys.argv[1]
     qase_token = sys.argv[2]
-    main(xml_file_path, qase_token)
+    main(xml_file_path, qase_token)  # Pass xml_file_path and qase_token to main function
+
+
+
+# import os
+# import requests
+# import xml.etree.ElementTree as ET
+
+# def update_test_case(test_case_data, qase_token):
+#     url = f"https://api.qase.io/v1/result/{test_case_data['RepositoryCode']}/{test_case_data['TestRunId']}"
+
+#     payload = {
+#         "status": test_case_data["Status"],
+#         "case_id": test_case_data["TestCaseId"]
+#     }
+    
+#     headers = {
+#         "accept": "application/json",
+#         "content-type": "application/json",
+#         "Token": qase_token
+#     }
+
+#     response = requests.post(url, json=payload, headers=headers)
+#     print(response.text)
+
+# def main():
+#     # Get xml_file_path and qase_token from environment variables
+#     xml_file_path = os.environ.get('XML_FILE_PATH')
+#     qase_token = os.environ.get('QASE_TOKEN')
+
+#     # Load and parse the XML file
+#     tree = ET.parse(xml_file_path)
+#     root = tree.getroot()
+
+#     for test_case_elem in root.findall('TestCase'):
+#         test_case_data = {
+#             "RepositoryCode": test_case_elem.find('RepositoryCode').text,
+#             "TestRunId": int(test_case_elem.find('TestRunId').text),
+#             "TestCaseId": int(test_case_elem.find('TestCaseId').text),
+#             "Status": test_case_elem.find('Status').text
+#         }
+#         update_test_case(test_case_data, qase_token)
+
+# if __name__ == "__main__":
+#     main()
+
+
+
