4147ee831292b6856a329d5038fd6a2da2c01d1d
AndrewLazare443 <andrew.lazare@data.com>
2023-07-31 10:21:43 -0400

    updated main.py to parse format of genereated xml

--- main.py
+++ main.py
@@ -46,34 +46,41 @@ def update_test_case(test_case_data, qase_token):
     response = requests.post(url, json=payload, headers=headers)
     print(response.text)
 
-def main(xml_file_path, qase_token, test_plan_id):
+def main(xml_file_path, qase_token):
     # Load and parse the XML file
     tree = ET.parse(xml_file_path)
     root = tree.getroot()
 
-    # Extract repository code from the XML
-    repository_code = root.find("TestCase/RepositoryCode").text
+    # Extract repository code and test plan ID from the XML
+    repository_code = root.find("test-case").get("name").split(".")[0]
+    test_plan_id = 4  # Replace this with the actual test plan ID from your XML or pass it as a command-line argument
 
     # Create a new test run and get the test run ID
     test_run_id = create_test_run(qase_token, repository_code, test_plan_id)
-    
+
     if not test_run_id:
         sys.exit(1)
 
-    for test_case_elem in root.findall('TestCase'):
-        test_case_data = {
-            "RepositoryCode": repository_code,
-            "TestRunId": test_run_id,
-            "TestCaseId": int(test_case_elem.find('TestCaseId').text),
-            "Status": test_case_elem.find('Status').text
-        }
+    for test_case_elem in root.findall('test-case'):
+        # Extract data from the <output> tag
+        output_data = test_case_elem.find('output').text.strip()
+        output_lines = output_data.split("\n")
+        test_case_data = {}
+        for line in output_lines:
+            key, value = line.strip().split("=")
+            test_case_data[key] = value
+
+        test_case_data["RepositoryCode"] = repository_code
+        test_case_data["TestRunId"] = test_run_id
+        test_case_data["TestCaseId"] = int(test_case_data["TestCaseId"])
+        test_case_data["Status"] = test_case_elem.get("result")
+
         update_test_case(test_case_data, qase_token)
 
 if __name__ == "__main__":
-    if len(sys.argv) < 4:
-        print("Usage: python main.py <path_to_xml_file> <qase_token> <test_plan_id>")
+    if len(sys.argv) < 3:
+        print("Usage: python main.py <path_to_xml_file> <qase_token>")
         sys.exit(1)
     xml_file_path = sys.argv[1]
     qase_token = sys.argv[2]
-    test_plan_id = int(sys.argv[3])
-    main(xml_file_path, qase_token, test_plan_id)
+    main(xml_file_path, qase_token)
